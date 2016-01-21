479c3581c5f3b21515840e6f9d004ca95726a8b4
naveenmarikar123 <naveenmarikar123@github.com>
2016-01-22 01:52:04 +0530

    Interleave Fixed.

--- src/DynamicProgramming/StringInterleave.java
+++ src/DynamicProgramming/StringInterleave.java
@@ -20,9 +20,9 @@ public class StringInterleave {
 		String a = "aaa";
 		String b = "aaa";
 		String c = "aaaaaa";
-	    test ("XY", "X", "XXY");
-	    test ("YX", "X", "XXY");
-	    test ("XXY", "XXZ", "XXXXZY");
+	    //test ("XY", "X", "XXY");
+	    //test ("YX", "X", "XXY");
+	    System.out.println(test ("XXY", "XXZ", "XXXXZY"));
 	}
 
 	private static boolean test(String a, String b, String c) {
@@ -32,55 +32,55 @@ public class StringInterleave {
 		boolean[][] mat = new boolean[a.length()+1][b.length()+1];
 		
 		//Base Case Filling.
-		
-		mat[0][0] = true;
-		
-		for(int i = 1 ; i < m ;i++)
+		int i = 0;
+		int j = 0;
+		try{
+		for(i = 0 ; i < n ; i++)
 		{
-			if(b.charAt(i-1)==c.charAt(i-1))
+			for(j = 0 ; j < m ; j++)
 			{
-				if(mat[0][i-1])
-					mat[0][i]=true;
-				else
-					mat[0][i]=false;
-			}
-			else
-				mat[0][i]=false;
-		}
-		
-		for(int i = 1;  i < n ; i++)
-		{
-			if(a.charAt(i-1)==c.charAt(i-1))
-			{
-				if(mat[i-1][0])
-					mat[i][0]=true;
-				else
-					mat[i][0]=false;
-			}
-			else
-				mat[i][0] = false;
-		}
-		
-	
-		int i = 0 ;
-		int j = 0 ;
-	
-		for(i = 1 ; i < n ; i++)
-		{
-			for(j = 1 ; j < m ; j++)
-			{
-				if(c.charAt(i+j-1) == a.charAt(i-1))
+				if(i==0 && j==0)
+					mat[i][j] = true;
+				else if(i==0)
 				{
-					mat[i][j]=mat[i-1][j];
+					if((b.charAt(j-1)==c.charAt(i+j-1)))
+					mat[i][j] = mat[i][j-1];
+					else
+						mat[i][j]=false;
 				}
-				else if(c.charAt(i+j-1)==b.charAt(j-1))
+				else if(j==0)
 				{
-					mat[i][j]=mat[i][j-1];
+					if((a.charAt(i-1)==c.charAt(i+j-1)))
+					mat[i][j] = mat[i-1][j];
+					else
+						mat[i][j]=false;
 				}
-				else
-					mat[i][j]=false;
+				else if( (a.charAt(i-1)==c.charAt(i+j-1)) && !(b.charAt(j-1)==c.charAt(i+j-1)) )
+				{
+					mat[i][j] = mat[i-1][j];
+				}
+				else if( (a.charAt(i-1)!=c.charAt(i+j-1)) && (b.charAt(j-1)==c.charAt(i+j-1)) )
+				{
+					mat[i][j] = mat[i][j-1];
+				}
+				else if((a.charAt(i-1)==c.charAt(i+j-1)) && (b.charAt(j-1)==c.charAt(i+j-1)))
+				{
+					mat[i][j] = mat[i-1][j] || mat[i][j-1];
+				}
+
 			}
-		}	
+		
+		}
+		}
+		catch(Exception e)
+		{
+			System.out.println(i+" "+j);
+			System.out.println(a.charAt(i-1));
+			System.out.println(b.charAt(j-1));
+			System.out.println(c.charAt(i+j-1));
+			
+			e.printStackTrace();
+		}
 		
 		return mat[n-1][m-1];
 	}
